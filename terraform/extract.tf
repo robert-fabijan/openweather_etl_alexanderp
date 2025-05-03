ecf29fdd2526eee29c996f7bdba4e3efeb86362e
Jowita Gogol <jowita.m.gogol@gmail.com>
2025-05-03 19:02:20 +0100

    fix(extract): update service account references and comment out workflow definitions

--- terraform/extract.tf
+++ terraform/extract.tf
@@ -108,96 +108,96 @@ resource "google_pubsub_topic" "extract_historical_weather" {
   name = "get_historical_weather_data"
 }
 #########################################################################################################
-##### workflow for extract functions
-resource "google_workflows_workflow" "test_workflow" {
-  name            = "test-workflow"
-  region          = "europe-central2"
-  description     = "A test workflow"
-  service_account = "test-account@totemic-client-447220-r1.iam.gserviceaccount.com"
+# ##### workflow for extract functions
+# resource "google_workflows_workflow" "test_workflow" {
+#   name            = "test-workflow"
+#   region          = "europe-central2"
+#   description     = "A test workflow"
+#   service_account = "test-account@totemic-client-447220-r1.iam.gserviceaccount.com"
 
-  source_contents = <<-EOF
-    - init:
-        assign:
-            - results : {} # result from each iteration keyed by table name
-            - functions:
-                - get_pollution_data
-                - get_weather_data
-    - runQueries:
-        parallel:
-            shared: [results]
-            for:
-                value: func
-                in: $${functions}
-                steps:
-                  - call_function:
-                      call: http.get
-                      args:
-                        url: $${"https://europe-central2-totemic-client-447220-r1.cloudfunctions.net/" + func + "_tf"}
-                        auth:
-                          type: OIDC
-                      result: function_response
-                  - publish_to_pubsub:
-                      call: googleapis.pubsub.v1.projects.topics.publish
-                      args:
-                        topic: $${"projects/totemic-client-447220-r1/topics/" + func + "-topic"}
-                        body:
-                          messages:
-                            - data: $${base64.encode(text.encode(function_response.body))}
-                      result: pubsub_response
-                  - returnResult:
-                      assign:
-                          - results[func]: $${pubsub_response}
-    - returnResults:
-        return: $${results}
-  EOF
+#   source_contents = <<-EOF
+#     - init:
+#         assign:
+#             - results : {} # result from each iteration keyed by table name
+#             - functions:
+#                 - get_pollution_data
+#                 - get_weather_data
+#     - runQueries:
+#         parallel:
+#             shared: [results]
+#             for:
+#                 value: func
+#                 in: $${functions}
+#                 steps:
+#                   - call_function:
+#                       call: http.get
+#                       args:
+#                         url: $${"https://europe-central2-totemic-client-447220-r1.cloudfunctions.net/" + func + "_tf"}
+#                         auth:
+#                           type: OIDC
+#                       result: function_response
+#                   - publish_to_pubsub:
+#                       call: googleapis.pubsub.v1.projects.topics.publish
+#                       args:
+#                         topic: $${"projects/totemic-client-447220-r1/topics/" + func + "-topic"}
+#                         body:
+#                           messages:
+#                             - data: $${base64.encode(text.encode(function_response.body))}
+#                       result: pubsub_response
+#                   - returnResult:
+#                       assign:
+#                           - results[func]: $${pubsub_response}
+#     - returnResults:
+#         return: $${results}
+#   EOF
 
-}
+# }
 ##### workflow for extract raw geo data
-resource "google_workflows_workflow" "geo_workflow" {
-  name            = "geo-workflow"
-  region          = "europe-central2"
-  description     = "Workflow exporting raw geo data to pubsub"
-  service_account = "test-account@totemic-client-447220-r1.iam.gserviceaccount.com"
+# resource "google_workflows_workflow" "geo_workflow" {
+#   name            = "geo-workflow"
+#   region          = "europe-central2"
+#   description     = "Workflow exporting raw geo data to pubsub"
+#   service_account = "kajous-serviceaccount-elexon@elexon-data-project.iam.gserviceaccount.com"
 
-  source_contents = <<-EOF
-    main:
-      params: [input]
-      steps:
-        - call_function:
-            call: http.get
-            args:
-              url: https://europe-central2-totemic-client-447220-r1.cloudfunctions.net/get_geo_data_tf
-              auth:
-                type: OIDC
-            result: function_response
-        - publish_to_pubsub:
-            call: googleapis.pubsub.v1.projects.topics.publish
-            args:
-              topic: projects/totemic-client-447220-r1/topics/get_geo_data-topic
-              body:
-                messages:
-                  - data: $${base64.encode(text.encode(function_response.body))}
-            result: pubsub_response
-        - return_results:
-            return: $${pubsub_response}
-  EOF
+#   source_contents = <<-EOF
+#     main:
+#       params: [input]
+#       steps:
+#         - call_function:
+#             call: http.get
+#             args:
+#               url: https://europe-central2-totemic-client-447220-r1.cloudfunctions.net/get_geo_data_tf
+#               auth:
+#                 type: OIDC
+#             result: function_response
+#         - publish_to_pubsub:
+#             call: googleapis.pubsub.v1.projects.topics.publish
+#             args:
+#               topic: projects/var.project_id/topics/get_geo_data-topic
+#               body:
+#                 messages:
+#                   - data: $${base64.encode(text.encode(function_response.body))}
+#             result: pubsub_response
+#         - return_results:
+#             return: $${pubsub_response}
+#   EOF
 
-}
+# }
 #########################################################################################################
 ##### schedules for extract functions
-resource "google_cloud_scheduler_job" "test_workflow" {
-  name        = "test-workflow-schedule"
-  description = "Trigger for the test-workflow"
-  schedule    = "0 * * * *"
-  time_zone   = "Europe/Warsaw"
-  http_target {
-    uri         = "https://workflowexecutions.googleapis.com/v1/${google_workflows_workflow.test_workflow.id}/executions"
-    http_method = "POST"
-    oauth_token {
-      service_account_email = "test-account@totemic-client-447220-r1.iam.gserviceaccount.com"
-    }
-  }
-}
+# resource "google_cloud_scheduler_job" "test_workflow" {
+#   name        = "test-workflow-schedule"
+#   description = "Trigger for the test-workflow"
+#   schedule    = "0 * * * *"
+#   time_zone   = "Europe/Warsaw"
+#   http_target {
+#     uri         = "https://workflowexecutions.googleapis.com/v1/${google_workflows_workflow.test_workflow.id}/executions"
+#     http_method = "POST"
+#     oauth_token {
+#       service_account_email = "kajous-serviceaccount-elexon@elexon-data-project.iam.gserviceaccount.com"
+#     }
+#   }
+# }
 #########################################################################################################
 ##### invoker permissions for extract functions
 resource "google_cloudfunctions2_function_iam_member" "invoker" {
@@ -208,7 +208,7 @@ resource "google_cloudfunctions2_function_iam_member" "invoker" {
   cloud_function = each.value.name
 
   role   = "roles/cloudfunctions.invoker"
-  member = "serviceAccount:test-account@totemic-client-447220-r1.iam.gserviceaccount.com"
+  member = "serviceAccount:kajous-serviceaccount-elexon@elexon-data-project.iam.gserviceaccount.com"
 }
 resource "google_cloudfunctions2_function_iam_member" "invoker_extract_last_month" {
   project        = google_cloudfunctions2_function.extract_historical_pollution.project
@@ -216,5 +216,5 @@ resource "google_cloudfunctions2_function_iam_member" "invoker_extract_last_mont
   cloud_function = google_cloudfunctions2_function.extract_historical_pollution.name
 
   role   = "roles/cloudfunctions.invoker"
-  member = "serviceAccount:test-account@totemic-client-447220-r1.iam.gserviceaccount.com"
+  member = "serviceAccount:kajous-serviceaccount-elexon@elexon-data-project.iam.gserviceaccount.com"
 }
